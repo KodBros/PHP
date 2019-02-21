032b9e20b736e5f8847704383ea2ca7b71ce02f7
yusufkorkmaz <32537154+yusufkorkmaz@users.noreply.github.com>
2019-02-21 14:25:14 +0300

    Birkaç yeni şey daha...

--- Aritmetik İşlemler ve Fonksiyon Kullanımı.php
+++ Aritmetik İşlemler ve Fonksiyon Kullanımı.php
@@ -6,7 +6,7 @@
     $a=20;
     $b=15;
     $toplam=$a+$b;
-    echo "a+b = "."$toplam </br>"; 
+    echo "a+b = "."$toplam </br></br>"; 
 
     function topla(){
         global $a;
@@ -15,7 +15,11 @@
         $x=$a-$b;
     }
     topla();
-    echo "a = ".$a."</br>"."b = ".$b."</br>"."x = ".$x;
+    echo "a = ".$a."</br>"."b = ".$b."</br></br>"."x = ".$x."</br></br>";
+
+    $t=10;
+    $t++;
+    echo "t = $t";
 ?>
 
 </body>

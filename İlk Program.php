7b6e97131d869090180a466d8956c06f8e5a23c4
yusufkorkmaz <32537154+yusufkorkmaz@users.noreply.github.com>
2019-02-21 13:47:59 +0300

    Ad Soyad ve Yaş değerlerini yazdırdık.

---
+++ İlk Program.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    echo "<h4>Bilgilerim</h4>";
+    $ad="Yusuf"; $soyad="KORKMAZ"; $yas=20;
+    echo "Adım : ".$ad."</br>"."Soyadım : ".$soyad."</br>"."Doğum Yılım : ".$yas;
+?>
+
+</body>
+</html>
